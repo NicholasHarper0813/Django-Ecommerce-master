b24b124fb35322a680a22ac9f563e7da62a03e3c
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-08 04:43:26 -0800

    Update azure.py

--- demo/azure.py
+++ demo/azure.py
@@ -6,10 +6,10 @@ ALLOWED_HOSTS += ['*']
 WSGI_APPLICATION = 'market.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
 ]
 
 DATABASES = {
@@ -26,5 +26,5 @@ DATABASES = {
 
 STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 AZURE_ACCOUNT_NAME = os.getenv('AZ_STORAGE_ACCOUNT_NAME')
-AZURE_CONTAINER = os.getenv('AZ_STORAGE_CONTAINER')
 AZURE_ACCOUNT_KEY = os.getenv('AZ_STORAGE_KEY')
+AZURE_CONTAINER = os.getenv('AZ_STORAGE_CONTAINER')
